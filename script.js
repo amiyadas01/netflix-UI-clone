c455dbc5bdb0b8b1cd074d4f43c2d660f6462c9b
amiyadas01 <amiyadas708@gmail.com>
2024-10-08 10:27:03 +0530

    add the faq button's ans by clinking by a smoth functionality

--- script.js
+++ script.js
@@ -1,123 +1,27 @@
-let firstAns = document.querySelector(".first-ans");
-let secondAns = document.querySelector(".second-ans");
-let thirdAns = document.querySelector(".third-ans");
-let fourthAns = document.querySelector(".fourth-ans");
-let fifthAns = document.querySelector(".fifth-ans");
-let sixthAns = document.querySelector(".sixth-ans");
-let faqOne = document.querySelector(".FAQ1")
-let faqTwo = document.querySelector(".FAQ2")
-let faqThree = document.querySelector(".FAQ3")
-let faqFour = document.querySelector(".FAQ4")
-let faqFive = document.querySelector(".FAQ5")
-let faqSix = document.querySelector(".FAQ6")
-let press1 = false;
-let press2 = false;
-let press3 = false;
-let press4 = false;
-let press5 = false;
-let press6 = false;
+const faqBtn = document.querySelectorAll(".FAQ-button");
+const faqAns = document.querySelectorAll(".FAQ-ans")
+const faqSvg = document.querySelectorAll(".FAQ-svg")
 const scroll = new LocomotiveScroll({
     el: document.querySelector('.wraper'),
     smooth: true
 });
 
-
-
-faqOne.addEventListener("click", () => {
-    if (!press1) {
-        firstAns.classList.remove("hide")
-       
-    }else{
-        firstAns.classList.add("hide")
-    }
-    press1 = !press1;
-    sixthAns.classList.add("hide")
-    fourthAns.classList.add("hide")
-    thirdAns.classList.add("hide")
-    secondAns.classList.add("hide")
-    fifthAns.classList.add("hide")
-   
+faqBtn.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        faqAns.forEach((answer, ansIdx) => {
+            if (index === ansIdx) {
+                answer.classList.toggle("hide");
+            } else {
+                answer.classList.add("hide");
+            }
+            
+        })
+        faqSvg.forEach((svg,svgIdx) => {
+            if (index === svgIdx) {
+                svg.classList.toggle("rotate");
+            } else {
+                svg.classList.remove("rotate");
+            }
+                        })
+    })
 })
-
-faqTwo.addEventListener("click", () => {
-  
-
-    if (!press2) {
-        secondAns.classList.remove("hide")
-    }else{
-        secondAns.classList.add("hide")
-    }
-  
-    press2 = !press2;
-    firstAns.classList.add("hide")
-    fifthAns.classList.add("hide")
-    sixthAns.classList.add("hide")
-    fourthAns.classList.add("hide")
-    thirdAns.classList.add("hide")
-})
-
-faqThree.addEventListener("click", () => {
-    if (!press3) {
-        thirdAns.classList.remove("hide")
-
-    }else{
-        thirdAns.classList.add("hide")
-    }
-  
-    press3 = !press3;
-    secondAns.classList.add("hide")
-    firstAns.classList.add("hide")
-    fifthAns.classList.add("hide")
-    sixthAns.classList.add("hide")
-    fourthAns.classList.add("hide")
-})
-
-faqFour.addEventListener("click", () => {
-    if (!press4) {
-        fourthAns.classList.remove("hide")
-
-    }else{
-        fourthAns.classList.add("hide")
-    }
-  
-    press4 = !press4;
-    secondAns.classList.add("hide")
-    firstAns.classList.add("hide")
-    fifthAns.classList.add("hide")
-    sixthAns.classList.add("hide")
-    thirdAns.classList.add("hide")
-})
-
-faqFive.addEventListener("click", () => {
-    if (!press5) {
-        fifthAns.classList.remove("hide")
-
-    }else{
-        fifthAns.classList.add("hide")
-    }
-  
-    press5 = !press5;
-    secondAns.classList.add("hide")
-    firstAns.classList.add("hide")
-    sixthAns.classList.add("hide")
-    fourthAns.classList.add("hide")
-    thirdAns.classList.add("hide")
-})
-
-faqSix.addEventListener("click", () => {
-    if (!press6) {
-        sixthAns.classList.remove("hide")
-
-    }else{
-        sixthAns.classList.add("hide")
-    }
-  
-    press6 = !press6;
-    firstAns.classList.add("hide")
-    fifthAns.classList.add("hide")
-    secondAns.classList.add("hide")
-    fourthAns.classList.add("hide")
-    thirdAns.classList.add("hide")
-})
-
-
